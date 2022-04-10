8c56fc35e23f958aed76710c3a39e0cd9d6a4047
HJ-C <swch56@naver.com>
2022-04-11 05:41:34 +0900

    3

---
+++ dist/interface.js
@@ -0,0 +1,15 @@
+// interface 특징
+// object 자료형의 타입을 보다 편리하게 지정가능
+// type alias 와 용도와 기능은 동일
+var a = { color: 'red', width: 100 };
+var 변수 = { name: 'kim', age: 90 };
+var 상품 = { brand: 'Samsung', sn: 1360, model: ['TV', 'phone'] };
+var cart = [{ product: '청소기', price: 7000 }, { product: '삼다수', price: 800 }];
+var obj = {
+    plus: function (a, b) {
+        return a + b;
+    },
+    minus: function (a, b) {
+        return a - b;
+    }
+};
